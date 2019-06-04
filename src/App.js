4eeb27b235748b9b731111b50edc39b633d03dd3
Janie <evgeniyaberseneva89@gmail.com>
2019-06-04 17:00:07 +0300

    router stcruture

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import ProductsPage from "./pages/ProductsPage";
+import SingleProducts from "./pages/SingleProducts";
+import CartPage from "./pages/CartPage";
+import DefaultPage from "./pages/DefaultPage";
 import "./App.css";
 
 function App() {
-  return <h1>hello from delivery service</h1>;
+  return (
+    <>
+      {/* navbar, side, cart */}
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/contact" component={ContactPage} />
+        <Route path="/products" exact component={ProductsPage} />
+        <Route path="/products/:id" exact component={SingleProducts} />
+        <Route path="/cart" component={CartPage} />
+        <Route component={DefaultPage} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
