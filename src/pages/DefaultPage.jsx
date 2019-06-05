f46525e894cee6b4900f3aa92bacf6e445ff3e92
Janie <evgeniyaberseneva89@gmail.com>
2019-06-05 15:58:32 +0300

    default page complete

--- src/pages/DefaultPage.jsx
+++ src/pages/DefaultPage.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
+import defaultBcg from "../img/images/defaultBcg.jpeg";
+import { Link } from "react-router-dom";
+import Hero from "./../components/Hero";
 
 class DefaultPage extends Component {
   state = {};
   render() {
-    return <>Hello from default page</>;
+    return (
+      <>
+        <Hero img={defaultBcg} title="404" max="true">
+          <h2 className="text-uppercase">page not found</h2>
+          <Link to="/" className="main-link" style={{ margin: "2rem" }}>
+            return home
+          </Link>
+        </Hero>
+      </>
+    );
   }
 }
 
