60da4293ff7654cf8ba02bd8681537a2273bcdfd
Janie <evgeniyaberseneva89@gmail.com>
2019-06-05 11:40:10 +0300

    NavBar complete

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,62 @@
 import React from "react";
+import { FaBars, FaCartPlus } from "react-icons/fa";
+import styled from "styled-components";
+import logo from "../img/images/logo.svg";
+import { ProductConsumer } from "../context/context";
 
 const NavBar = () => {
   return (
-    <>
-      <h1>hello from navbar</h1>
-    </>
+    <ProductConsumer>
+      {value => {
+        const { cartItems, handleSideBar, handleCart } = value;
+        return (
+          <NavWrapper>
+            <div className="nav-center">
+              <FaBars className="nav-icon" onClick={handleSideBar} />
+              <img src={logo} alt="company logo" />
+              <div className="nav-cart">
+                <FaCartPlus className="nav-icon" onClick={handleCart} />
+                <div className="cart-items">{cartItems}</div>
+              </div>
+            </div>
+          </NavWrapper>
+        );
+      }}
+    </ProductConsumer>
   );
 };
 
 export default NavBar;
+
+const NavWrapper = styled.nav`
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+  width: 100%;
+  padding: 1rem 1.5rem;
+  background: var(--mainGrey);
+  border-bottom: 3px solid var(--mainBlue);
+  .nav-center {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    max-width: 1170px;
+    margin: 0 auto;
+  }
+  .nav-icon {
+    font-size: 1.5rem;
+    cursor: pointer;
+  }
+  .nav-cart {
+    position: relative;
+  }
+  .cart-items {
+    background: var(--mainBlue);
+    color: var(--mainWhite);
+    font-size: 0.8rem;
+    position: absolute;
+    top: -8px;
+    right: -8px;
+    border-radius: 50%;
+  }
+`;
