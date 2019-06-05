b6cafe61cfc362175db0b2df32d9c8e1fac63224
Janie <evgeniyaberseneva89@gmail.com>
2019-06-05 15:15:53 +0300

    Hero component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
-import { ProductConsumer } from "../context/context";
+import Hero from "../components/Hero";
+import { Link } from "react-router-dom";
 
 class HomePage extends Component {
   state = {};
   render() {
     return (
-      <ProductConsumer>
-        {value => {
-          console.log(value);
-          return <h1>hello from homepgae</h1>;
-        }}
-      </ProductConsumer>
+      <>
+        <Hero title="awesome gadgets" max="true">
+          <Link to="/products">our products</Link>
+        </Hero>
+      </>
     );
   }
 }
