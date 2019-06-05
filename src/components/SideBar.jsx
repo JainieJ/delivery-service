36ca68ebcb4b8ab8ba2ea923dd46d8942187f94e
Janie <evgeniyaberseneva89@gmail.com>
2019-06-05 13:12:20 +0300

    SideBar complete

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,11 +1,70 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context/context";
 
 const SideBar = () => {
   return (
-    <>
-      <h1>hello from SideBar</h1>
-    </>
+    <ProductConsumer>
+      {value => {
+        const { links, sideBarOpen, handleSideBar } = value;
+        return (
+          <SideWrapper show={sideBarOpen}>
+            <ul>
+              {links.map(link => {
+                return (
+                  <li key={link.id}>
+                    <Link
+                      className="sidebar-link"
+                      to={link.path}
+                      onClick={handleSideBar}
+                    >
+                      {link.text}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </SideWrapper>
+        );
+      }}
+    </ProductConsumer>
   );
 };
 
 export default SideBar;
+
+const SideWrapper = styled.nav`
+  position: fixed;
+  top: 61px;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: var(--mainGrey);
+  z-index: 1;
+  border-right: 4px solid var(--mainBlue);
+  transition: var(--mainTransition);
+  transform: ${props => (props.show ? "translateX(0)" : "translateX(-100%)")};
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  .sidebar-link {
+    display: block;
+    font-size: 1.5rem;
+    text-transform: uppercase;
+    color: var(--mainBlack);
+    padding: 0.5rem 1.5rem;
+    background: transparent;
+    transition: var(--mainTransition);
+  }
+  .sidebar-link:hover {
+    background: var(--mainBlue);
+    color: var(--mainWhite);
+    padding-left: 2.5rem;
+    text-decoration: none;
+  }
+  @media (min-width: 576px) {
+    width: 20rem;
+  }
+`;
