545de61af4cad7e47b6dd5ec9edaa1d8c17ed331
Janie <evgeniyaberseneva89@gmail.com>
2019-06-06 12:15:51 +0300

    componentDidMount and setProducts methods set up

--- src/context/context.js
+++ src/context/context.js
@@ -18,9 +18,49 @@ class ProductProvider extends Component {
     filteredProducts: [],
     featuredProducts: [],
     singleProduct: {},
-    loading: true,
+    loading: false,
     links: linkData,
     socialIcons: socialData
+  };
+  componentDidMount() {
+    //getting products from contentful
+
+    this.setProducts(items);
+  }
+  setProducts = products => {
+    //converting to readable array
+    let storeProducts = products.map(item => {
+      const { id } = item.sys;
+      const product = { id, ...item.fields };
+      return product;
+    });
+    //filter be featured
+    let featuredProducts = storeProducts.filter(item => item.featured);
+    console.log(featuredProducts);
+    //setting state
+    this.setState({
+      storeProducts,
+      featuredProducts,
+      filteredProducts: storeProducts,
+      cart: this.getStorageCart(),
+      singleProduct: this.getStorageProduct(),
+      loading: false
+    });
+  };
+  getStorageCart = () => {
+    return [];
+  };
+  getStorageProduct = () => {
+    return {};
+  };
+  getTotals = () => {};
+  addTotals = () => {};
+  syncStorage = () => {};
+  addToCart = id => {
+    console.log("add to cart", id);
+  };
+  setSingleProduct = id => {
+    console.log("set signle product", id);
   };
   handleSideBar = () => {
     this.setState({ sideBarOpen: !this.state.sideBarOpen });
@@ -42,7 +82,9 @@ class ProductProvider extends Component {
           handleSideBar: this.handleSideBar,
           handleCart: this.handleCart,
           closeCart: this.closeCart,
-          openCart: this.openCart
+          openCart: this.openCart,
+          addToCart: this.addToCart,
+          setSingleProduct: this.setSingleProduct
         }}
       >
         {this.props.children}
