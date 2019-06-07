2c60ac7b96f28f4ed03338a3d734b2cc9a9e5f5c
Janie <evgeniyaberseneva89@gmail.com>
2019-06-07 18:00:41 +0300

    CartList complete

--- src/components/CartPage/CartList.jsx
+++ src/components/CartPage/CartList.jsx
@@ -1,8 +1,42 @@
 import React from "react";
 import CartItem from "./CartItem";
+import { ProductConsumer } from "../../context/context";
 
 const CartList = () => {
-  return <CartItem />;
+  return (
+    <div className="container-fluid">
+      {/* row */}
+      <div className="row">
+        <div className="col">
+          <ProductConsumer>
+            {value => {
+              const { cart, increment, decrement, removeItem } = value;
+              if (cart.length === 0) {
+                return (
+                  <h1 className="text-title text-center my-4">
+                    your cart is empty
+                  </h1>
+                );
+              }
+              return (
+                <>
+                  {cart.map(item => (
+                    <CartItem
+                      key={item.id}
+                      cartItem={item}
+                      increment={increment}
+                      decrement={decrement}
+                      removeItem={removeItem}
+                    />
+                  ))}
+                </>
+              );
+            }}
+          </ProductConsumer>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartList;
