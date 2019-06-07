39de52fc05c5d5fdc8204d8d15c7e261ffa0692b
Janie <evgeniyaberseneva89@gmail.com>
2019-06-07 16:54:28 +0300

    CartColumns

--- src/components/CartPage/Cart.jsx
+++ src/components/CartPage/Cart.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import CartColumns from "./CartColumns";
+import Title from "../../components/Title";
 import CartList from "./CartList";
 import CartTotals from "./CartTotals";
 
 const Cart = () => {
   return (
-    <>
+    <section className="py-5">
+      {/* title */}
+      <div className="container">
+        <Title title="cart items" center="true" />
+      </div>
+      {/* cart contents */}
       <CartColumns />
       <CartList />
       <CartTotals />
-    </>
+    </section>
   );
 };
 
--- src/components/CartPage/CartColumns.jsx
+++ src/components/CartPage/CartColumns.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const CartColumns = () => {
-  return <h1>hello from CartColumns</h1>;
+  return (
+    <div className="container-fluid text-center d-none d-lg-block my-5">
+      <div className="row">
+        <div className="col-lg-2">
+          <p className="text-uppercase">products</p>
+        </div>
+        <div className="col-lg-2">
+          <p className="text-uppercase">name of product</p>
+        </div>
+        <div className="col-lg-2">
+          <p className="text-uppercase">price</p>
+        </div>
+        <div className="col-lg-2">
+          <p className="text-uppercase">quantity</p>
+        </div>
+        <div className="col-lg-2">
+          <p className="text-uppercase">remove</p>
+        </div>
+        <div className="col-lg-2">
+          <p className="text-uppercase">total</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartColumns;
