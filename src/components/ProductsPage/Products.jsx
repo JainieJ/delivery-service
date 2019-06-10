1e56fa90b1a0f96901c404483738917930572e6e
Janie <evgeniyaberseneva89@gmail.com>
2019-06-10 14:05:41 +0300

    products component structure with filtering

--- src/components/ProductsPage/Products.jsx
+++ src/components/ProductsPage/Products.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ProductConsumer } from "../../context/context";
 import Title from "./../Title";
 import Product from "./../Product";
+import ProductsFilter from "./ProductsFilter";
 
 const Products = () => {
   return (
@@ -12,10 +13,24 @@ const Products = () => {
           <section className="py-5">
             <div className="container">
               <Title title="our products" center="true" />
+              <ProductsFilter />
+              <div className="row">
+                <div className="col-10 mx-auto">
+                  <h6 className="text-title">
+                    total products: {filteredProducts.length}
+                  </h6>
+                </div>
+              </div>
               <div className="row py-5">
-                {filteredProducts.map(product => (
-                  <Product key={product.id} product={product} />
-                ))}
+                {filteredProducts.length === 0 ? (
+                  <div className="col text-title text-center">
+                    sorry, no items matched your search
+                  </div>
+                ) : (
+                  filteredProducts.map(product => (
+                    <Product key={product.id} product={product} />
+                  ))
+                )}
               </div>
             </div>
           </section>
---
+++ src/components/ProductsPage/ProductsFilter.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductsFilter = () => {
+  return <h1>filtering products</h1>;
+};
+
+export default ProductsFilter;
